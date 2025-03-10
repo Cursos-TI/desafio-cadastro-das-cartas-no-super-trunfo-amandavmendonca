4919dd7cee29bddf96c7a7607b6c85557992bc98
amandavmendonca <amandavanmendonca@hotmail.com>
2025-03-10 11:05:01 +0000

    DESAFIO SUPER TRUNFO - NÍVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,7 @@ int main()
 
     char codCarta[3], codCarta2[3];
     int populacao, numPonTuris, populacao2, numPonTuris2;
-    float area, pib, area2, pib2;
+    float area, pib, area2, pib2, densiPop, densiPop2, pibPerC, pibPerC2;
 
     // Coleta de dados carta 1
 
@@ -36,15 +36,22 @@ int main()
     printf("Digite o PIB da cidade: ");
     scanf("%f", &pib2);
 
+    densiPop = populacao / area;
+    densiPop2 = populacao2 / area2;
+
+    pibPerC = pib / populacao;
+    pibPerC2 = pib2 / populacao2;
+
     // Saída de dados carta 1
 
     printf("\nCódigo Carta 1: %s\nNúmero da População: %i\nPontos turísticos: %i\nÁrea: %f\nPIB: %f\n",
            codCarta, populacao, numPonTuris, area, pib);
+    printf("Densidade populacional: %f\nPIB Per Capita: %f\n", densiPop, pibPerC);
 
     // Saída de dados carta 2
 
-    printf("\nCódigo Carta 2: %s\nNúmero da População: %i\nPontos turísticos: %i\nÁrea: %f\nPIB: %f",
+    printf("\nCódigo Carta 2: %s\nNúmero da População: %i\nPontos turísticos: %i\nÁrea: %f\nPIB: %f\n",
            codCarta2, populacao2, numPonTuris2, area2, pib2);
-
+    printf("Densidade populacional: %f\nPIB Per Capita: %f\n", densiPop2, pibPerC2);
     return 0;
 }
